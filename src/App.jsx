be2969c19177ae1950f25981ae258908b19e021f
rahydyn <kanaokasatoru@gmail.com>
2022-10-02 03:23:35 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./css/App.css";
+import "./css/Home.css";
+// import { UnderTabBar } from './UnderTabBar';
+import { RouterConfig } from "./RouterConfig";
+// For test
+import { TestFirebase } from "./test/TestFirebase";
+
+function App() {
+  return (
+    <div className="phone_size">
+      <ChakraProvider>
+        <BrowserRouter>
+          <RouterConfig />
+        </BrowserRouter>
+      </ChakraProvider>
+      <TestFirebase/>
+    </div>
+  );
+}
+// {さとるだよ}
+
+export default App;
---
+++ src/components/templates/UnderTabBar.jsx
@@ -0,0 +1,56 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { FeedContents } from '../feedPages/FeedContents'
+import { FriendsContents } from '../friendPages/FriendsContents'
+import { HomeContents } from '../homePages/HomeContents'
+import { NewFriendModal } from '../friendPages/NewFriendModal'
+import { Flex, Spacer } from '@chakra-ui/react'
+import { Link, } from "react-router-dom";
+import { Avatar, Wrap, WrapItem } from '@chakra-ui/react'
+import { MdHome } from "react-icons/md";
+import { MdOutlineArticle } from "react-icons/md";
+import { MdGroup } from "react-icons/md";
+
+
+
+
+export function UnderTabBar() {
+    return (
+        <>
+            <div className='top_bar'>
+                <Flex>
+                    <Link to="/" className='service_name'>Profile3.0</Link>
+                    <Spacer />
+
+                    <Link to="/MyProfilePage">
+                        <Wrap className='top_profile_icon'>
+                            <WrapItem>
+                                <Avatar name='Dan Abrahmov' />
+                            </WrapItem>
+
+                        </Wrap>
+                    </Link>
+                </Flex>
+            </div>
+            <Tabs variant='soft-rounded' colorScheme='whiteAlpha' isFitted='true' className='under_tab_bar'>
+                <TabPanels>
+                    <TabPanel>
+                        <HomeContents />
+                    </TabPanel>
+                    <TabPanel>
+                        <FeedContents />
+                    </TabPanel>
+                    <TabPanel>
+                        <p className='friend'>Friend</p>
+                        <FriendsContents />
+                        <NewFriendModal />
+                    </TabPanel>
+                </TabPanels>
+                <TabList className='under_nav_bar'>
+                    <Tab color={"yellow.50"}><MdHome />Home</Tab>
+                    <Tab color={"yellow.50"}><MdOutlineArticle />Feed</Tab>
+                    <Tab color={"yellow.50"}><MdGroup />Friend</Tab>
+                </TabList>
+            </Tabs>
+        </>
+    )
+}
